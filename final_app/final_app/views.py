2314bfdebccd4fc14dc0b8fa5b3865939242d57b
DabuEdison <202210022@gordoncollege.edu.ph>
2025-05-09 02:17:19 +0800

    User

--- final_app/final_app/views.py
+++ final_app/final_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from ..accounts.forms import RegisterForm, LoginForm
+from ..accounts.forms import RegisterForm, LoginForm # type: ignore
 
 def register_view(request):
     if request.method == 'POST':
